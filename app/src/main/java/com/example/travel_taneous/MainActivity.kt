142934f25abed9b85e9e8d47aacc2672f5715da8
Ada <ada@Adas-MacBook-Air.local>
2020-01-07 12:57:35 -0800

    Login button goes to Dashboard page

--- app/src/main/java/com/example/travel_taneous/MainActivity.kt
+++ app/src/main/java/com/example/travel_taneous/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.travel_taneous
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loginBtn.setOnClickListener {
+            val dashboardIntent = Intent(this, DashboardActivity::class.java)
+            startActivity(dashboardIntent )
+        }
     }
+
+
 }
